23dcd4a4be9bb3d829b4200bc53d825c1441014a
Adam Albu <45970222+adamalbu@users.noreply.github.com>
2026-03-11 07:57:21 +0000

    lasers :)

---
+++ player/laser.gd
@@ -0,0 +1,21 @@
+extends RayCast2D
+
+@export
+var lasers: Array[Node2D]
+
+func _draw() -> void:
+	if Input.is_action_pressed("fire") and self.is_colliding() && get_viewport_rect().has_point(get_viewport().canvas_transform * self.get_collision_point()):
+	# if Input.is_action_pressed("fire"):
+		for laser in lasers:
+			var origin = to_local(laser.global_position)
+			var hit = to_local(self.get_collision_point())
+			draw_line(origin, hit, Color.GREEN, 1.0)
+
+func _process(_delta: float) -> void:
+	queue_redraw()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+# func _physics_process(_delta: float) -> void:
+# 	if Input.is_action_pressed("fire"):
+# 		if self.is_colliding():
+# 			print("collidint")
