5a06f5ef5fa8ae7bdb4f5884ce3002a4e13f19af
Adam Albu <45970222+adamalbu@users.noreply.github.com>
2026-03-13 15:43:29 +0000

    particles

--- player/laser.gd
+++ player/laser.gd
@@ -3,17 +3,28 @@ extends RayCast2D
 @export
 var lasers: Array[Node2D]
 
+@onready
+var mine_particles = $MineParticles
+@onready var laser_particles = $LaserParticles
+
 func _draw() -> void:
 	if Input.is_action_pressed("fire") and self.is_colliding() && get_viewport_rect().has_point(get_viewport().canvas_transform * self.get_collision_point()):
 		for laser in lasers:
 			var origin = to_local(laser.global_position)
-			var hit = to_local(self.get_collision_point())
+			var hit = to_local(get_collision_point())
 			draw_line(origin, hit, Color.GREEN, 1.0)
 
 func _process(delta: float) -> void:
 	queue_redraw()
 
 	if Input.is_action_pressed("fire") and self.is_colliding() && get_viewport_rect().has_point(get_viewport().canvas_transform * self.get_collision_point()):
+		mine_particles.global_position = get_collision_point()
+		mine_particles.emitting = true
+		laser_particles.global_position = get_collision_point()
+		laser_particles.emitting = true
 		var object = get_collider()
 		if object is Object:
 			get_collider().health -= 10 * delta
+	else:
+		mine_particles.emitting = false
+		laser_particles.emitting = false
