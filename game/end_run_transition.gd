8b48cd2c5d8b30d3158630d8daf9753e1738194a
Adam Albu <45970222+adamalbu@users.noreply.github.com>
2026-04-09 14:01:22 +0100

    scene change

--- game/end_run_transition.gd
+++ game/end_run_transition.gd
@@ -11,6 +11,8 @@ func animate() -> void:
 	added_money_label.text = "₡" + str(GameState.run_money)
 	set_display_total_money(GameState.money)
 	animation_player.play("part_1")
+	await animation_player.animation_finished
+	get_tree().change_scene_to_file("res://upgrade/upgrade_ui.tscn")
 
 func tween_add_money() -> void:
 	var tween = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
